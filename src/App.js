09b997b6cdc464b0230a2249bfd4b6f3e79b1388
marcello120 <sarosi.marcell@gmail.com>
2021-10-03 22:43:11 +0200

    it works
    it wroks now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Page from './components/Page';
 import Auth from './components/Auth';
@@ -8,13 +8,11 @@ import React from 'react';
 
 function App() {
 
-  const users = ["John", "Tom","Jeff"]
-
   return (
     <Router>
         <Route path="/" exact render={(props) => (
           <>
-            <Auth users = {users}></Auth>
+            <Auth></Auth>
           </>
         )}>
         </Route>
--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
-
   </React.StrictMode>,
   document.getElementById('root')
 );
